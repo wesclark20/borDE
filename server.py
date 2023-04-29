ec2946ccf51b29885dbae7df990d6a2fe8225a21
wesclark20 <wesclark@udel.edu>
2023-04-29 19:29:04 -0400

    "website launches, without displaying frontend"

--- server.py
+++ server.py
@@ -1,9 +1,26 @@
-from bottle import route, run, template, Bottle, static_file
+from bottle import Bottle, static_file, run, route, post, request
+from sqlalchemy import create_engine, Column, Integer, String, text
 
 app = Bottle()
 
 @app.route('/')
 def home():
-    return static_file('index.html', root='.\\borde\public\index.html')
+    return static_file('index.html', root='.\\borde\public')
 
-run(app, host='localhost', port=8080)
+@app.route('/<filename:path>')
+def serve_static(filename):
+    return static_file(filename, root='.\\borde\src')
+
+@app.post('/login')
+def do_login():
+    pass
+
+@app.post('/signup')
+def do_signup():
+    pass
+
+@app.post('/post_event')
+def do_event():
+    pass
+
+run(app, host='localhost', port=8080, debug=True)
