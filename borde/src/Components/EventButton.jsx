523fc24a1b0cba1801869363d81d812a5f3064f9
Ian Duffy <ianduffy@udel.edu>
2023-04-29 17:27:32 -0400

    Event Button Changes

--- borde/src/Components/EventButton.jsx
+++ borde/src/Components/EventButton.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-//import Button from "react-bootstrap/Button";
+import Button from "react-bootstrap/Button";
 //import Stack from "react-bootstrap/Stack";
 
 function EventButton() {
     return (
-        <button type="button" class="btn btn-info btn-lg btn-block">Create DEvent</button>
+        <Button variant="info">Create DEvent </Button>
 );
 }
 export default EventButton;
