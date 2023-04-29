5381bdd9053b159428bb399725e6ecf5b0f44e02
Ian Duffy <ianduffy@udel.edu>
2023-04-29 15:47:01 -0400

    Event Card and Profile Card changes

--- borde/src/Components/ProfileCard.jsx
+++ borde/src/Components/ProfileCard.jsx
@@ -1,52 +1,21 @@
-import React from 'react';
-import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardTitle, MDBCardText, MDBCardBody, MDBCardImage, MDBBtn } from 'mdb-react-ui-kit';
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-export default function ProfileCard() {
+function ProfileCard() {
   return (
-    <div className="vh-100" style={{ backgroundColor: '#9de2ff' }}>
-      <MDBContainer>
-        <MDBRow className="justify-content-center">
-          <MDBCol md="9" lg="7" xl="5" className="mt-5">
-            <MDBCard style={{ borderRadius: '15px' }}>
-              <MDBCardBody className="p-4">
-                <div className="d-flex text-black">
-                  <div className="flex-shrink-0">
-                    <MDBCardImage
-                      style={{ width: '180px', borderRadius: '10px' }}
-                      src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp'
-                      alt='Generic placeholder image'
-                      fluid />
-                  </div>
-                  <div className="flex-grow-1 ms-3">
-                    <MDBCardTitle>Danny McLoan</MDBCardTitle>
-                    <MDBCardText>Senior Journalist</MDBCardText>
-
-                    <div className="d-flex justify-content-start rounded-3 p-2 mb-2"
-                      style={{ backgroundColor: '#efefef' }}>
-                      <div>
-                        <p className="small text-muted mb-1">Articles</p>
-                        <p className="mb-0">41</p>
-                      </div>
-                      <div className="px-3">
-                        <p className="small text-muted mb-1">Followers</p>
-                        <p className="mb-0">976</p>
-                      </div>
-                      <div>
-                        <p className="small text-muted mb-1">Rating</p>
-                        <p className="mb-0">8.5</p>
-                      </div>
-                    </div>
-                    <div className="d-flex pt-1">
-                      <MDBBtn outline className="me-1 flex-grow-1">Chat</MDBBtn>
-                      <MDBBtn className="flex-grow-1">Follow</MDBBtn>
-                    </div>
-                  </div>
-                </div>
-              </MDBCardBody>
-            </MDBCard>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
-    </div>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Ian Duffy</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Newark, DE</Card.Subtitle>
+        <Card.Text>
+          Sophomore at the University of Delaware studying Computer Science and Cybersecurity.
+        </Card.Text>
+        <Button variant="primary">Visit Profile</Button>
+      </Card.Body>
+    </Card>
   );
 }
+
+export default ProfileCard;
