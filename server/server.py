ac58e7afb7264ae5008170083bacc57bdbf5fb25
Michael Lutz <mikelutz2400@gmail.com>
2023-04-30 04:18:02 -0400

    Render events based on json

--- server/server.py
+++ server/server.py
@@ -1,47 +1,45 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 import json
-from os import path
-from . import models
+# from os import path
 
 app = Flask(__name__)
-CORS(app, origins=['http://localhost:3000', 'http://localhost:5000'])
+CORS(app, origins=['http://localhost:3000'])
 
 
 # ---=== SQLAlchemy Stuff ===---#
-db = SQLAlchemy()
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-db.init_app(app)
+# db = SQLAlchemy()
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+# db.init_app(app)
 
 
-def create_database(app):
-    if not path.exists('database.db'):
-        db.create_all(app=app)
-        print('Created Database')
+# def create_database(app):
+#     if not path.exists('database.db'):
+#         db.create_all(app=app)
+#         print('Created Database')
 
 # ---=== End SQLAlchemy nonsense ===---#
 
 
-@app.route("/members")
-def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
-
-
-@app.route('/api', methods=['POST'])
+@app.route('/api', methods=['GET', 'POST'])
 def api():
-    with open("data.json", "r") as f:
-        stored = json.load(f)
+    if request.method == 'POST':
+        with open("data.json", "r") as f:
+            stored = json.load(f)
 
-        newReport = json.loads(request.data)
-        stored['events'].append(newReport)
+            newReport = json.loads(request.data)
+            stored['events'].append(newReport)
 
-        print("stored:", stored)
-    with open("data.json", "w") as f:
-        json.dump(stored, f)
-    return "Working"
+            print("stored:", stored)
+        with open("data.json", "w") as f:
+            json.dump(stored, f)
+    if request.method == 'GET':
+        with open("data.json", "r") as data:
+            return data.read()
+    return None
 
 
 if __name__ == "__main__":
-    create_database(app)
+    # create_database(app)
     app.run(debug=True)
