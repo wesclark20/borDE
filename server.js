b22a9a9ec7675a63f700ddc0eaa563a8d984b725
wesclark20 <wesclark@udel.edu>
2023-04-29 21:33:14 -0400

    "added bodyparse and cors"

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
