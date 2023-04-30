f57b45a1cc6c97fea718ee40508fd13a68cc3043
wesclark20 <wesclark@udel.edu>
2023-04-30 00:11:55 -0400

    "file paths may be fixed"

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
-from flask import Flask, send_file, send_from_directory
+from flask import Flask, send_file, send_from_directory, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def serveIndex():
-    return send_file('build/index.html')
+    return send_file('borde\\build\index.html')
 
-@app.route('/build/<path:path')
+@app.route('/build/<path:path>')
 def serveStatic(path):
-    return send_from_directory('static', path)
+    return send_from_directory('borde\\build\static', path)
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8080, debug=True)
