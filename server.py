fcfcded8693c4ae62b1d78b0533b51114f7e30e3
wesclark20 <wesclark@udel.edu>
2023-04-29 23:49:21 -0400

    "Maybe frontend will work now?"

--- server.py
+++ server.py
@@ -3,8 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return '<h1>Hello, World!</h1>'
+def serveIndex():
+    return app.send_static_file('.\\build\index.html')
+
+@app.route('/static/<filename:path')
+def serveStatic(filename):
+    return app.send_static_file(f'.\\build\{filename}')
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8080, debug=True)
