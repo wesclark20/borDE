ec149c55c390f0cfafc5810b91c6f169f849076b
Michael Lutz <mikelutz2400@gmail.com>
2023-04-30 06:42:45 -0400

    test data and images

--- borde/src/Components/ProfileCard.jsx
+++ borde/src/Components/ProfileCard.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Image from "react-bootstrap/Image";
 
 function ProfileCard(props) {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={props.img} />
+    <Card style={{ width: "18rem" }}>
+      <Image
+        src={require("../Images/misery-club-penguin.gif")}
+        alt="picture of penguin"
+      />
+
       <Card.Body>
         <Card.Title>{props.name}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">{props.email}</Card.Subtitle>
-        <Card.Text>
-          {props.bio}
-        </Card.Text>
+        <Card.Text>{props.bio}</Card.Text>
         <Button variant="primary">Visit Profile</Button>
       </Card.Body>
     </Card>
