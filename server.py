235c28299720ee69179f1eb870521fbd84f9fab4
wesclark20 <wesclark@udel.edu>
2023-04-29 23:53:40 -0400

    "fix maybe"

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, send_file, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def serveIndex():
-    return app.send_static_file('.\\build\index.html')
+    return send_file('static/index.html')
 
-@app.route('/static/<filename:path')
-def serveStatic(filename):
-    return app.send_static_file(f'.\\build\{filename}')
+@app.route('/static/<path:path')
+def serveStatic(path):
+    return send_from_directory('static', path)
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8080, debug=True)
