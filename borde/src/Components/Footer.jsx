fc16821dcd8f2f612dfca02d0f96dc8b717759d2
Ian Duffy <ianduffy@udel.edu>
2023-04-30 10:54:51 -0400

    Added Ian Duffy Profile Photo

--- borde/src/Components/Footer.jsx
+++ borde/src/Components/Footer.jsx
@@ -27,7 +27,7 @@ function Footer() {
           <Col>
             <ProfileCard
               name={"Ian Duffy"}
-              img={require("../Images/misery-club-penguin.gif")}
+              img={require("../Images/")}
               email={"ianduffy@udel.edu"}
               bio={"Sophomore Computer Science: Cyber Security Student"}
             />
