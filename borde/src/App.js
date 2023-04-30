3d109b7f67bbd879802de52fb8c41a7fefdd60a5
Michael Lutz <mikelutz2400@gmail.com>
2023-04-30 01:31:49 -0400

    Can get server requests

--- borde/src/App.js
+++ borde/src/App.js
@@ -1,7 +1,20 @@
 import Navigationbar from "./Components/Navigationbar";
 import Body from "./Components/Body";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setdata] = useState({});
+
+  // Using useEffect for single rendering
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/members", { mode: "cors" })
+      .then((res) => res.json())
+      .then((data) => {
+        setdata(data);
+        console.log(data);
+      });
+  }, []);
+
   return (
     <>
       <Navigationbar />
