dfc5c618858c7c9f1adf26bf81f8924f63cfa0fa
wesclark20 <wesclark@udel.edu>
2023-04-30 01:49:31 -0400

    "database models.py"

--- server/server.py
+++ server/server.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
+#---=== SQLAlchemy Stuff ===---#
+db = SQLAlchemy()
 
 app = Flask(__name__)
 CORS(app)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db.init_app(app)
 
 @app.route("/members")
 def members():
