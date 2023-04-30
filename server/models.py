edc43ac1ffd1d5fbb84e36c38bf7cff09d2fc996
wesclark20 <wesclark@udel.edu>
2023-04-30 04:21:57 -0400

    "cleaned up sql stuff"

--- server/models.py
+++
@@ -1,9 +0,0 @@
-from server import db
-
-class UserPost(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
-    date = db.Column(db.String)
-    time = db.Column(db.String)
-    description = db.Column(db.String)
-    email = db.Column(db.String)
--- server/server.py
+++ server/server.py
@@ -1,28 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
 import json
-from os import path
-import models as models
 
 app = Flask(__name__)
 CORS(app, origins=['http://localhost:3000', 'http://localhost:5000'])
-
-
-# ---=== SQLAlchemy Stuff ===---#
-db = SQLAlchemy()
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-db.init_app(app)
-
-
-def create_database(app):
-    if not path.exists('database.db'):
-        with app.app_context():
-            db.create_all()
-        print('Created Database')
-create_database(app)
-
-# ---=== End SQLAlchemy nonsense ===---#
 
 
 @app.route("/members")
@@ -34,17 +15,6 @@ def members():
 def api():
     with open("data.json", "r") as f:
         stored = json.load(f)
-        
-    #take the data from the resulting dictionary above
-        title = stored['title']
-        date = stored['time']
-        time = stored['time']
-        description = stored['description']
-        email = stored['email']
-    #create a user post and add it to the database
-        userPost = models.UserPost(title=title, date=date, time=time, description=description, email=email)
-        db.session.add(userPost)
-        db.session.commit()
 
         newReport = json.loads(request.data)
         stored['events'].append(newReport)
